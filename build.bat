144a4b6dbc29264912ee5c8aa7be237e40b2a41f
Sentropic <60368610+Sentropic@users.noreply.github.com>
2020-12-23 04:12:37 -0600

    Removed VT variability
    Untangled VT from Craftec assets into external zip
    Updated builder

--- build.bat
+++ build.bat
@@ -1,5 +1,10 @@
+@echo off
 del "Craftec.zip"
-"C:\Program Files\7-Zip\7z" a -tzip -r "%cd%\Craftec.zip" "%cd%\*" -x!"*.git*" -x!*"Craftec.zip" -x!"build.bat" -x!"*.cubik" -x!"*.bbmodel"
+"C:\Program Files\7-Zip\7z" x "VanillaTweaks.zip" -o"%cd%\temp" -r -aoa
+ren "%cd%\temp\Selected Packs.txt" "Vanilla Tweaks.txt"
+"C:\Program Files\7-Zip\7z" a "%cd%\Craftec.zip" "%cd%\temp\*"
+rmdir "%cd%\temp\" /Q/S
+"C:\Program Files\7-Zip\7z" a "%cd%\Craftec.zip" "%cd%\*" -xr@buildexclude.txt
 echo.
 echo [92mSuccesfully built resource pack![0m
 pause

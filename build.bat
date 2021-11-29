84a094f4b15ab06acbca43f1f6742c2a31e8b2b6
Sentropic <sentropic.sv@gmail.com>
2021-11-28 19:19:54 -0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,13 +1,11 @@
 @echo off
 set "repPath=%cd%"
-cd ..
-set "rpPath=%cd%"
-cd %repPath%
+set "rpPath=%AppData%\.minecraft\resourcepacks\"
 rmdir "%rpPath%\Craftec\" /Q/S
-"C:\Program Files\7-Zip\7z" x "VanillaTweaks.zip" -o"%rpPath%\Craftec" -r -aoa
+7z x "VanillaTweaks.zip" -o"%rpPath%\Craftec" -r -aoa
 ren "%rpPath%\Craftec\Selected Packs.txt" "Vanilla Tweaks.txt"
 Xcopy "%repPath%" "%rpPath%\Craftec" /S/Y/Q /EXCLUDE:buildexclude.txt
 del "%repPath%\Craftec.zip"
-"C:\Program Files\7-Zip\7z" a "%repPath%\Craftec.zip" "%rpPath%\Craftec\*"
+7z a "%repPath%\Craftec.zip" "%rpPath%\Craftec\*"
 echo.
 echo [92mSuccesfully built resource pack![0m

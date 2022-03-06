c27d07218b3814bff60f20d895f3abca25a4d0d0
Sentropic <sentropic.sv@gmail.com>
2022-03-05 23:19:09 -0600

    Refactor to CrystalPledge

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Craftec Resource Pack
+# Crystal Pledge Resource Pack
 ## How to build:
 - In order to build the resource pack, you must first install 7-zip: https://www.7-zip.org/download.html
 - Then, add the directory where 7-zip is installed to your system's PATH variable.
 - Now you can run build.bat!
 
-For convenience, and fast reloading in Minecraft (Using F3+T), the batch file will generate a folder resourcepack in your .minecraft/resourcepacks folder, as well as a Craftec.zip file inside the repo.
+For convenience, and fast reloading in Minecraft (Using F3+T), the batch file will generate a folder resourcepack in your .minecraft/resourcepacks folder, aside from a CrystalPledge.zip file inside the repo.
--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 set "repPath=%cd%"
 set "rpPath=%AppData%\.minecraft\resourcepacks\"
-rmdir "%rpPath%\Craftec\" /Q/S
-7z x "VanillaTweaks.zip" -o"%rpPath%\Craftec" -r -aoa
-ren "%rpPath%\Craftec\Selected Packs.txt" "Vanilla Tweaks.txt"
-Xcopy "%repPath%" "%rpPath%\Craftec" /S/Y/Q /EXCLUDE:buildexclude.txt
-del "%repPath%\Craftec.zip"
-7z a "%repPath%\Craftec.zip" "%rpPath%\Craftec\*"
+rmdir "%rpPath%\CrystalPledge\" /Q/S
+7z x "VanillaTweaks.zip" -o"%rpPath%\CrystalPledge" -r -aoa
+ren "%rpPath%\CrystalPledge\Selected Packs.txt" "Vanilla Tweaks.txt"
+Xcopy "%repPath%" "%rpPath%\CrystalPledge" /S/Y/Q /EXCLUDE:buildexclude.txt
+del "%repPath%\CrystalPledge.zip"
+7z a "%repPath%\CrystalPledge.zip" "%rpPath%\CrystalPledge\*"
 echo.
 echo [92mSuccesfully built resource pack![0m
